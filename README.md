d73e9accbe6ec7ef6e899c0c9512982bef07c2a3
ecokova <elena.cokova@tufts.edu>
2016-03-20 00:10:28 -0400

    completed tutorial, game shell begun

---
+++ README.md
@@ -0,0 +1,5 @@
+#Joust
+
+###References
+Font - http://www.dafont.com/minecraft.font?l[]=10&l[]=1
+Images - http://seanriddle.com/willy2.html
---
+++ Tutorial_for_reference/joust_game.rb
@@ -0,0 +1,14 @@
+require 'rubygems'
+require 'gosu'
+
+class JoustGame < Gosu::Window
+  def initialize
+  end
+
+  def update
+  end
+
+  def draw 
+  end
+
+end
